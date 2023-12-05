1d5a0ede2fba25e229cc5edad069c6594bc0c2ec
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-12-05 12:22:20 -0500

    Brutal!

--- components/guitarra/guitarra.jsx
+++ components/guitarra/guitarra.jsx
@@ -16,20 +16,20 @@ export default function Guitarra({ guitarra }) {
 
   return (
     <div className={styles.guitarra}>
-      
       <Image
         src={imagen.data.attributes.formats.small.url}
         alt={`Imagen de ${nombre}`}
-        width={200}
+        width={600}
         height={200}
-        
       />
 
       <div className={styles.contenido}>
         <h3>{nombre}</h3>
         <p className={styles.descripcion}>{texto}</p>
         <p className={styles.precio}>${precio}</p>
-        <Link href={`/guitarras/${url}`} className={styles.enlace}>Ver Producto</Link>
+        <Link href={`/guitarras/${url}`} className={styles.enlace}>
+          Ver Producto
+        </Link>
       </div>
     </div>
   );
