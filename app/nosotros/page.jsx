ace3f9fdfcb1f1342a3a73fe4af3d2904066e167
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-12-05 06:29:59 -0500

    Nosotros a full

--- app/nosotros/page.jsx
+++ app/nosotros/page.jsx
@@ -1,11 +1,41 @@
-import Link from 'next/link';
 import LayoutPrincipal from '@/components/layoutPrincipal/layoutPrincipal';
+import Image from 'next/image';
+import styles from './nosotros.module.scss';
 
 export default function Nosotros() {
   return (
     <>
       <LayoutPrincipal>
-        <h1>Nosotros</h1>
+        <main className="contenedor">
+          <h2 className="heading">Nosotros</h2>
+
+          <div className={styles.nosotros}>
+            <div>
+              <Image
+                src="/img/nosotros.jpg"
+                alt="Imagen de nosotros"
+                width={1000}
+                height={800}
+              />
+            </div>
+
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error
+                dicta nesciunt sequi repellendus unde minus odit, sunt quis
+                labore, perferendis consequuntur fuga ipsa, eius vel omnis
+                beatae minima dignissimos maiores.
+              </p>
+
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error
+                dicta nesciunt sequi repellendus unde minus odit, sunt quis
+                labore, perferendis consequuntur fuga ipsa, eius vel omnis
+                beatae minima dignissimos maiores.
+              </p>
+            </div>
+          </div>
+        </main>
       </LayoutPrincipal>
     </>
   );
